6d7ad78dc46bd2294c34d129d0dfa56b87aabae8
yslolly <anneleen.tijtgat@outlook.com>
2021-02-23 14:09:40 +0100

    Add project files.

---
+++ WebApiOef/Controllers/NumberController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiOef.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NumberController : ControllerBase
+    {
+        string file = @"Desktop\getal.txt";
+
+        [HttpGet]
+        public ActionResult<int> GetNumber()
+        {
+            if (File.Exists(file))
+            {
+                string[] lines = File.ReadAllLines(file);
+                return Ok(lines);
+            }
+            return NotFound();
+        }
+
+        [HttpPost("number")]
+
+
+        [HttpPost("random number")]
+
+
+
+    }
+}
